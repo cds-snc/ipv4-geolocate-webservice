529f2b831cd36053390a5017c5b0245c61498de9
Jimmy Royer <jimleroyer@gmail.com>
2024-01-10 22:11:34 -0500

    Load maxmind db at server startup only

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use std::env;
 
 use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::sync::Arc;
 
 use http_body_util::Full;
 use hyper::{ Request, Response, StatusCode };
@@ -12,22 +13,20 @@ use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper_util::rt::TokioIo;
-use maxminddb::geoip2;
+use maxminddb::{ geoip2, Reader };
 use regex::Regex;
 use std::str::FromStr;
 use std::net::IpAddr;
 use tokio::net::TcpListener;
 
 async fn location(
-    req: Request<hyper::body::Incoming>
+    req: Request<hyper::body::Incoming>,
+    reader: Arc<Reader<Vec<u8>>>
 ) -> Result<Response<Full<Bytes>>, Infallible> {
     let uri = req.uri().to_string();
-
-    let path = env::var("MMDB_PATH").unwrap_or("./GeoLite2-City.mmdb".to_string());
-    let reader = maxminddb::Reader::open_readfile(path).unwrap();
     let set = Regex::new(r#"/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"#).unwrap();
-
     let ip_str;
+
     if uri == "/" {
         // TODO: Add in Remote IP
         ip_str = "127.0.0.1";
@@ -71,23 +70,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let port: u16 = env::var("PORT").unwrap_or("8080".to_string()).parse().unwrap();
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-
     let listener = TcpListener::bind(addr).await?;
     println!("Listening on http://{}", addr);
+
+    let path = env::var("MMDB_PATH").unwrap_or("./GeoLite2-City.mmdb".to_string());
+    let reader = maxminddb::Reader::open_readfile(path.clone()).unwrap();
+    let reader = Arc::new(reader);
+    println!("Loaded maxmind city locations database from {}.", path);
+
     loop {
         let (stream, _) = listener.accept().await?;
 
         // Use an adapter to access something implementing `tokio::io` traits as if they implement
         // `hyper::rt` IO traits.
         let io = TokioIo::new(stream);
+        let reader_clone = Arc::clone(&reader);
 
         // Spawn a tokio task to serve multiple connections concurrently
         tokio::task::spawn(async move {
             // Finally, we bind the incoming connection to our services
             if
-                let Err(err) = http1::Builder
-                    ::new()
-                    .serve_connection(io, service_fn(location)).await
+                let Err(err) = http1::Builder::new().serve_connection(
+                    io,
+                    service_fn(move |req| location(req, Arc::clone(&reader_clone)))
+                ).await
             {
                 println!("Error serving connection: {:?}", err);
             }
