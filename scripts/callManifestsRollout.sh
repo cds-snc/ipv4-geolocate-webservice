de4eddd2a47d9d94081ae34f69e2f35eeea31792
Jimmy Royer <jimleroyer@gmail.com>
2024-06-06 15:47:55 -0400

    Remove the git sha ID parameter for the gh action workflow

--- scripts/callManifestsRollout.sh
+++ scripts/callManifestsRollout.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
 # Parameters that need to be passed in:
-GITHUB_SHA=$1
-RELEASE_TAG=$2
-
-# Check if the required parameters are passed in
-if [ -z "$GITHUB_SHA" ]; then
-  echo "ERROR: GITHUB_SHA is not set"
-  exit 1
-fi
+RELEASE_TAG=$1
 
 if [ -z "$RELEASE_TAG" ]; then
   echo "ERROR: RELEASE_TAG is not set"
   exit 1
 fi
 
-PAYLOAD="{\"ref\":\"main\",\"inputs\":{\"docker_sha\":\"$GITHUB_SHA\",\"release_tag\":\"$RELEASE_TAG\"}}"
+PAYLOAD="{\"ref\":\"main\",\"inputs\":{\"release_tag\":\"$RELEASE_TAG\"}}"
 
 RESPONSE=$(curl -w '%{http_code}\n' \
   -o /dev/null -s \
